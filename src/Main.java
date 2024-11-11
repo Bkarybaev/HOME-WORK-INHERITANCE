3f232ff9dc7fb4245db6aaa75a9f8fa8aba26fa2
Bkarybaev <baielkarybaev@gmail.com>
2024-11-11 12:32:51 +0600

    abstracsia

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Programmer programmer = new Programmer("Aibek","programmer","google");
-        Dancer dancer = new Dancer("Ernist","dancer","BTS");
-        Singer singer = new Singer("Amir","Sinder","Biz tobu");
+        Person programmer = new Programmer("Aibek","programmer","google");
+        Person dancer = new Dancer("Ernist","dancer","BTS");
+        Person singer = new Singer("Amir","Sinder","Biz tobu");
         Object[] objects = new Person[]{programmer,dancer,singer};
         for (Object object : objects) {
             System.out.println(object.toString());
